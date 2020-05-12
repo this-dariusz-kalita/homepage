cdee1d0ffa49297434a7f4e80bbefe8bf38cc8b4
Dariusz Kalita <dariusz.kalita1@gmail.com>
2020-05-12 18:55:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName= 'Józek';
+const age= '42';
+
+
+
+alert('Witaj na mojej stronie :)');
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lata`);
